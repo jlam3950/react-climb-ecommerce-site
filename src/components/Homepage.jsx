5030d55532ae0fa56bd0e77a679a8fb448c08883
Jeffrey Lamar <jlam3950@gmail.com>
2022-08-19 09:36:14 -0400

    added bootstrap carousel

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import Hero from '/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/images/Hero.jpg'
+import React from "react";
+import Hero from "/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/images/Hero.jpg";
+import Hero1 from "/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/images/Hero1.jpg";
+import Hero2 from "/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/images/Hero2.jpg";
+import Carousel from 'react-bootstrap/Carousel';
 
 const Homepage = () => {
- 
   return (
-    <> 
-      <img src= {Hero} className ='img-fluid hero'  alt ='hero_img'></img>
+    <>
+      <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel_splash"
+          src={Hero1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h1 class = 'hero_header'> Climb Shop </h1>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel_splash"
+          src={Hero}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel_splash"
+          src={Hero2}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </>
-  )
-}
+  );
+};
 
 export default Homepage;
