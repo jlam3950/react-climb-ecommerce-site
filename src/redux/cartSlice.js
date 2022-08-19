77b55f25c792ac05739481ab2277e29a078ab43c
Jeffrey Lamar <jlam3950@gmail.com>
2022-08-19 09:37:01 -0400

    updated cartSlice with additional reducers.

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -3,7 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
  cart: [],
- totalPrice: 0, 
+ favList: [], 
+ totalPrice: 0,
+ count: 0,
+ favorites: 0,
 }
 
 const cartSlice = createSlice({
@@ -11,18 +14,96 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      state.cart.push(action.payload);
+      const item = state.cart.find(
+        product => product.id === action.payload.id
+        )
+
+      if(item){
+        console.log(item.quantity);
+        item.quantity += 1;
+      }
+      else if(!item){
+        state.cart.push(action.payload);
+        
+      } 
+      state.count += 1;
       state.totalPrice += action.payload.price;
     },
     removeFromCart: (state, action) => {
-      state.cart = state.cart.filter((x) => x.id !== action.payload.id)
-      console.log(action.payload.price);
-      state.totalPrice -= action.payload.price;
-    },
-  }
-});
+      const item = state.cart.find(
+        product => product.id === action.payload.id
+        )
 
+      if(item){
+        state.totalPrice -= action.payload.price * item.quantity;
+        state.count -= 1 * item.quantity;
+      } else {
+        state.count -= 1;
+      }
+      
+      state.cart = state.cart.filter((x) => x.id !== action.payload.id)
+    },
+    incrementQty: (state, action) => {
+      const item = state.cart.find(
+        product => product.id === action.payload.id
+        )
+
+      item.quantity += 1; 
+      state.count += 1;
+      state.totalPrice += action.payload.price
+      },
+    decrementQty: (state, action) => {
+      const item = state.cart.find(
+        product => product.id === action.payload.id
+        )
+
+      if(item){
+        item.quantity -= 1;
+        state.count -=1;
+        state.totalPrice -= action.payload.price
+      } 
+      
+      if (item.quantity === 0)
+          state.cart = state.cart.filter((x) => x.id !== action.payload.id)
+      },
+      saveToFavorites: (state, action) => {
+        const item = state.favList.find(
+          product => product.id === action.payload.id
+          )
+  
+        if(item){
+          alert('This is already in the cart.');
+          // state.favList.push(action.payload);
+        }
+        else if(!item){
+          state.favList.push(action.payload);
+          state.favorites += 1;
+        } 
+      },
+      removeFromFavorites: (state, action) => {
+        const item = state.favList.find(
+          product => product.id === action.payload.id
+          )
+  
+        if(item){
+          state.favList = state.favList.filter((x) => x.id !== action.payload.id)
+          state.favorites -=1;
+        } 
+      },
+    }
+  });
+
+export const SelectFavList = (state) => state.cartContainer.favList;
+export const SelectFav = (state) => state.cartContainer.favorites;
+export const SelectCount = (state) => state.cartContainer.count;
 export const SelectPrice = (state) => state.cartContainer.totalPrice;
 export const SelectCart = (state) => state.cartContainer.cart;
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { 
+  addToCart, 
+  removeFromCart, 
+  incrementQty, 
+  decrementQty,
+  saveToFavorites,
+  removeFromFavorites, 
+} = cartSlice.actions;
 export default cartSlice.reducer;
--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -1,23 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
 
-const initialState = {
-    count: 0,
-}
+    // const initialState = {
+    //     count: 0,
+    // }
 
-export const quantitySlice = createSlice({
-    name: 'quantity',
-    initialState,
-    reducers: {
-        increment(state){
-            state.count += 1
-        },
-        decrement: (state) => {
-            state.count -= 1
-        },
-    }
-});
+    // export const quantitySlice = createSlice({
+    //     name: 'quantity',
+    //     initialState,
+    //     reducers: {
+    //         increment(state,){
+    //             state.count += 1
+    //         },
+    //         decrement: (state) => {
+    //             state.count -= 1
+    //         },
+    //     }
+    // });
 
-export const selectQuantity = (state) => state.quantity.count;
-export const { increment, decrement } = quantitySlice.actions;
-export default quantitySlice.reducer;
+    // export const selectQuantity = (state) => state.quantity.count;
+    // export const { increment, decrement } = quantitySlice.actions;
+    // export default quantitySlice.reducer;
