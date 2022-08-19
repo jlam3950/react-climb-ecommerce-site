a5fc2e1bce90c6dfaacfacf38392d9d71547f3ea
Jeffrey Lamar <jlam3950@gmail.com>
2022-08-19 09:35:13 -0400

    updated local storage to include add items, nanoid

--- src/LocalStorage.js
+++ src/LocalStorage.js
@@ -1,13 +1,36 @@
+import { nanoid } from '@reduxjs/toolkit';
 import climbingRope from '/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/images/climbingRope.jpg';
+import climbingRope2 from '/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/images/red_climbRope.jpg';
+import climbingRope3 from '/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/images/climbingRope_neon.jpg';
+
 
 const LocalStorage = [ 
     { 
-    id: 0,
+    id: nanoid(),
     quantity: 1,
-    name: 'rope',
+    name: 'blue rope',
     price: 199.99,
     image: climbingRope,
-    description: 'why would you climb without a rope?'
+    description: 'why would you climb without a rope?',
+    favorite: false,
+    },
+    { 
+    id: nanoid(),
+    quantity: 1,
+    name: 'red rope',
+    price: 249.99,
+    image: climbingRope2,
+    description: 'why blue, when you can go red. pick me.',
+    favorite: false,
+    },
+    { 
+    id: nanoid(),
+    quantity: 1,
+    name: 'neon rope',
+    price: 49.99,
+    image: climbingRope3,
+    description: 'neon means danger. do you live dangerously?',
+    favorite: false,
     },
 ]
 
