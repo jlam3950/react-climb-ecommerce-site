352552fa583611b180f3b217f7212b10976dc835
Jeffrey Lamar <jlam3950@gmail.com>
2022-08-20 12:41:30 -0400

    added onClick. searchbar functionality for shop page.

--- src/components/Gear.jsx
+++ src/components/Gear.jsx
@@ -1,28 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import LocalStorage from "/Users/Jeff/Desktop/personalprojects/climbing-ecommerce/src/LocalStorage.js";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { FaHeart } from "react-icons/fa";
-// import { SelectFav } from "../redux/cartSlice";
-import { 
-  addToCart,
-  saveToFavorites,
-} from '../redux/cartSlice';
+import { addToCart, saveToFavorites } from "../redux/cartSlice";
 
 const Gear = () => {
-  // const [itemCount, setItemCount] = useState(0);
-  // // onClick = {() => setCount(count - 1)}
-  // const favQuantity = useSelector(SelectFav);
+  const [searchProd, setSearchProd] = useState("");
   const dispatch = useDispatch();
-  
+
   const addProductToCart = (product) => {
     dispatch(addToCart(product));
-  }
+  };
 
   const favoritesBtnEvent = (product) => {
-    // let status = product.favorite;
-    // product.favorite = !product.favorite;
+    product.favorite = !product.favorite;
     dispatch(saveToFavorites(product));
-  }
+  };
 
   return (
     <div>
@@ -33,27 +26,70 @@ const Gear = () => {
             <h1>Essential Climbing Gear</h1>
           </div>
           <hr></hr>
+          <input
+            className="mx-auto"
+            style={{ width: "50vw" }}
+            type="text"
+            placeholder="Search Products"
+            onChange={(event) => {
+              setSearchProd(event.target.value);
+            }}
+          ></input>
         </div>
       </div>
-      <div className="container mx-auto container_gear d-flex justify-content-center" style = {{width: '100vw'}}>
+      <div
+        className="container mx-auto container_gear d-flex justify-content-center"
+        style={{ width: "100vw" }}
+      >
         <div className="row">
-          {LocalStorage.map((product, idx) => {
+          {LocalStorage.filter((input) => {
+            if (searchProd === "") {
+              return input;
+            } else if (
+              input.name.toLowerCase().includes(searchProd.toLowerCase())
+            ) {
+              return input;
+            }
+          }).map((product, idx) => {
             return (
-              <div className="card mx-3 my-3" style={{ width: "18rem" }} key = {idx}>
+              <div
+                className="card mx-3 my-3"
+                style={{ width: "18rem" }}
+                key={idx}
+              >
                 <img
                   className="card-img-top"
-                  src= {product.image}
+                  src={product.image}
                   alt={product.title}
                 />
                 <div className="card-body">
                   <h5 className="card-title">{product.name}</h5>
-                  <span><h6>price: {product.price} </h6> </span>
-                  <p className="card-text">
+                  <span>
+                    <h6>price: {product.price} </h6>{" "}
+                  </span>
+                  <p className="card-text card_description">
                     {product.description}
                   </p>
-                  <button className="btn btn-success mx-2" onClick = {() => {addProductToCart(product)}}>Add to Cart</button>
-                  <button className="btn btn-primary mx-2" onClick = {() => {favoritesBtnEvent(product)}}> 
-                  <FaHeart className = { product.favorite === true ? 'selected' : 'notSelected'}/></button>
+                  <button
+                    className="btn btn-success mx-2"
+                    onClick={() => {
+                      addProductToCart(product);
+                    }}
+                  >
+                    Add to Cart
+                  </button>
+                  <button
+                    className="btn btn-primary mx-2"
+                    onClick={() => {
+                      favoritesBtnEvent(product);
+                    }}
+                  >
+                    <FaHeart
+                      className={
+                        product.favorite === true ? "selected" : "notSelected"
+                      }
+                    />
+                  </button>
                 </div>
               </div>
             );
