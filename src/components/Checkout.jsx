ab66503e9b8caeb4b88ad42d42c12f1394ddf651
Jeffrey Lamar <jlam3950@gmail.com>
2022-08-20 12:42:15 -0400

    added Paypal payment

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-// import { useSelector } from 'react-redux';
 import CartList from "../features/cart/cartList";
-import { useSelector } from 'react-redux';
-import { 
-  SelectPrice, 
-} from '../redux/cartSlice'
+import { useSelector } from "react-redux";
+import { SelectPrice } from "../redux/cartSlice";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 const Checkout = () => {
+  let price = useSelector(SelectPrice);
+  let client_id = process.env.REACT_APP_CLIENT_ID;
+  console.log(useSelector(SelectPrice));
+
   return (
     <>
-        <div className="container py-4">
+      <div className="container py-4">
         <div className="row">
           <div className="col"></div>
           <div>
@@ -25,9 +27,35 @@ const Checkout = () => {
         <div className="row">
           <CartList />
         </div>
-        
       </div>
-      <h3 className = {useSelector(SelectPrice) === 0 ? 'hidePrice' : ''}> Total Price: ${useSelector(SelectPrice)} </h3>
+      <h3 className={useSelector(SelectPrice) === 0 ? "hidePrice" : ""}>
+        {" "}
+        Total Price: ${useSelector(SelectPrice).toFixed(2)}{" "}
+      </h3>
+      <div className="checkout_payment mx-auto my-3">
+        <div></div>
+        <PayPalScriptProvider options={client_id} forceReRender={price}>
+          <PayPalButtons
+            forceReRender={price}
+            createOrder={(data, actions) => {
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    amount: {
+                      value: price, 
+                    },
+                  },
+                ],
+              });
+            }}
+            onApprove={async (data, actions) => {
+              const details = await actions.order.capture();
+              const name = details.payer.name.given_name;
+              alert("Thanks for your purchase, " + name);
+            }}
+          />
+        </PayPalScriptProvider>
+      </div>
     </>
   );
 };
