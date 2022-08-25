24423d31eec8da888425ece156c36be4257bb5b1
Jeffrey Lamar <jlam3950@gmail.com>
2022-08-24 23:57:27 -0400

    updated navbar with hamburger menu. update f link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 // import { faFacebook } from '@fortawesome/free-solid-svg-icons';
-import { FaFacebook } from 'react-icons/fa'
-import { FaInstagram} from 'react-icons/fa'
-import { FaTwitter } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
-
-
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -20,28 +18,38 @@ const Footer = () => {
             className="btn btn-primary btn-lg btn-floating mx-2 footerIcon"
             style={{ background: "#54456b" }}
           >
-           < FaFacebook />
+            <a className="text-light" href="https://facebook.com">
+              <FaFacebook />
+            </a>
           </button>
           <button
             type="button"
             className="btn btn-primary btn-lg btn-floating mx-2 footerIcon"
             style={{ background: "#54456b" }}
           >
-            < FaInstagram />
+            {" "}
+            <a className="text-light" href="https://instagram.com">
+              <FaInstagram />
+            </a>
           </button>
           <button
             type="button"
             className="btn btn-primary btn-lg btn-floating mx-2 footerIcon"
             style={{ background: "#54456b" }}
           >
-            < FaTwitter />
+            <a className="text-light" href="https://twitter.com">
+              <FaTwitter />
+            </a>
           </button>
           <button
             type="button"
             className="btn btn-primary btn-lg btn-floating mx-2 footerIcon"
             style={{ background: "#54456b" }}
+            href="https://github.com/jlam3950"
           >
-            < FaGithub />
+            <a className="text-light" href="https://github.com/jlam3950">
+              <FaGithub />
+            </a>
           </button>
         </div>
         <div
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,27 +2,61 @@ import React from "react";
 import { NavLink } from 'react-router-dom';
 import ShopCart from './buttons/ShopCart';
 import FavoritesBtn from './buttons/FavoritesBtn';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
+const NavBar = () => {
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-lights container-fluid py-1 px-3">
+    <Navbar bg="light" expand="xl" className = 'mx-0 row'>
+      <Container className = 'mx-0'>
+        <Navbar.Brand>
+        <>
+          <NavLink className= "navbar-brand fw-bold rightNav" to="/">
+            <>Climb Shop</>
+          </NavLink>
+          <ShopCart /> 
+          <FavoritesBtn />
+        </>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink className="nav-link" to="/about">
+                About
+            </NavLink>
+            <NavLink className="nav-link" to="/contact">
+                Contact
+            </NavLink>
+            <NavLink className="nav-link" to="/gear">
+                Shop
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-lights py-1 px-3">
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon" onClick = {() => clickToggle()}></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <NavLink className="nav-link" to="/">
+              <NavLink className="nav-link" to="/"> 
                 Home 
               </NavLink>
             </li>
@@ -41,19 +75,19 @@ const Navbar = () => {
                 Shop
               </NavLink>
             </li>
-            
           </ul>
+          
         </div>
-        <div>
-          <NavLink className="navbar-brand fw-bold rightNav" to="/">
+        <div className = { hideTitle === true ? 'rightNav_hide' : ''}>
+          <NavLink className= "navbar-brand fw-bold rightNav" to="/">
             <>Climb Shop</>
           </NavLink>
           <ShopCart /> 
           <FavoritesBtn />
         </div>
-      </nav>
+      </nav> */}
     </>
   );
 };
 
-export default Navbar;
+export default NavBar;
